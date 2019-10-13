93d7db0263202b8dc1bc298a04ad64460accb3da
LetItCode <letitcode@ya.ru>
2019-10-13 04:16:14 +0300

    fix required spaces

--- examples/book-database-bot.js
+++ examples/book-database-bot.js
@@ -4,19 +4,19 @@ const { Extra, Markup } = Telegraf
 const { Widget, sendWidget } = TelegrafWidget
 
 const booksDB = [
-  {id: '1', title: 'Book one', description: 'Book one description...'},
-  {id: '2', title: 'Book two', description: 'Book two description...'},
-  {id: '3', title: 'Book three', description: 'Book three description...'},
-  {id: '4', title: 'Book four', description: 'Book four description...'},
-  {id: '5', title: 'Book five', description: 'Book five description...'},
-  {id: '6', title: 'Book six', description: 'Book six description...'}
+  { id: '1', title: 'Book one', description: 'Book one description...' },
+  { id: '2', title: 'Book two', description: 'Book two description...' },
+  { id: '3', title: 'Book three', description: 'Book three description...' },
+  { id: '4', title: 'Book four', description: 'Book four description...' },
+  { id: '5', title: 'Book five', description: 'Book five description...' },
+  { id: '6', title: 'Book six', description: 'Book six description...' }
 ]
 
 const booksWidget = new Widget('mybooks', 'list')
 
 booksWidget.on('list', ({ reply, editMessageText, widget }) => {
   const buttons = booksDB.map((book) => booksWidget.button(book.title, 'short-info', book.id))
-  const extra = Markup.inlineKeyboard(buttons, {columns: 2}).extra()
+  const extra = Markup.inlineKeyboard(buttons, { columns: 2 }).extra()
   return widget.data
     ? editMessageText('Ok\nChoose a book:', extra)
     : reply('Ok\nChoose a book:', extra)
@@ -47,7 +47,7 @@ booksWidget.on('full-info', (ctx) => {
   const extra = Extra.markdown().markup(Markup.inlineKeyboard([
     booksWidget.button('Show short info', 'short-info', ctx.widget.query),
     booksWidget.button('Go go books list', 'list')
-  ], {columns: 1}))
+  ], { columns: 1 }))
   return ctx.editMessageText(text, extra)
 })
 
